b2d9515f3b7db6361f332cc788acf16751feae76
LeePyeongBok <pblee@cnuglobal.com>
2020-02-27 18:30:28 +0900

    1차 디자인 템플릿 적용 #2
    1. vue 기본설정 경로 구현
     - 먼가 불안요소가 있어보이는데 일단 단행

--- DNPService/vue/vue.config.js
+++ DNPService/vue/vue.config.js
@@ -1,24 +1,28 @@
+/**
+ * https://cli.vuejs.org/config 에서 설정값 체크할것
+*/
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  pluginOptions: {
-    electronBuilder: {
-      builderOptions: {
-        appId: 'dnp',
-        asar: false,
-        productName: 'DNP',
-        win: {
-          target: [
-            {
-              target: 'portable',
-              arch: [
-                'x64',
-              ],
-            },
-          ],
-        },
-      },
-    },
-  },
+	// publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/dnp',
+	publicPath: "/dnp/vue",
+	"transpileDependencies": [
+		"vuetify"
+	],
+	pluginOptions: {
+		electronBuilder: {
+		builderOptions: {
+			appId: 'dnp',
+			asar: false,
+			productName: 'DNP',
+			win: {
+				target: [
+					{
+						target: 'portable',
+						arch: ['x64'],
+					}
+				]
+			}
+		}
+		}
+	}
 }
