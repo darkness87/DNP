218ca045c04227ec8336542d4b663d9ddd53c514
LeePyeongBok <pblee@cnuglobal.com>
2020-02-28 07:18:01 +0900

    1차 디자인 템플릿 적용 #2
    1. 불필요한 파일 제거
     - background.js는 electron 기반의 설정으로 확인됨
    2. 출력경로 수정
    
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

--- DNPService/vue/vue.config.js
+++ DNPService/vue/vue.config.js
@@ -4,25 +4,29 @@
 
 module.exports = {
 	// publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/dnp',
-	publicPath: "/dnp/vue",
+	publicPath: "/dnp",
+	outputDir : "../src/main/webapp",		// package.json의 설정된 경로값이 우선적으로 적용 default : "dist"
+	assetsDir : "",								// css, js 등의 파일의 폴더 경로를 제어함 default : ""
+	indexPath : "index.html",					// 메인페이지의 위치와 파일명을 지정가능 기본경로는 outputDir에 의존한다. default : "index.html"
+	
 	"transpileDependencies": [
 		"vuetify"
 	],
 	pluginOptions: {
 		electronBuilder: {
-		builderOptions: {
-			appId: 'dnp',
-			asar: false,
-			productName: 'DNP',
-			win: {
-				target: [
-					{
-						target: 'portable',
-						arch: ['x64'],
-					}
-				]
+			builderOptions: {
+				appId: 'dnp',
+				asar: false,
+				productName: 'DNP',
+				win: {
+					target: [
+						{
+							target: 'portable',
+							arch: ['x64'],
+						}
+					]
+				}
 			}
-		}
 		}
 	}
 }
